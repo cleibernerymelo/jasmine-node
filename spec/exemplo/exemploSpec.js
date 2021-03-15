d8e143c1c262de1af87114664f0cf3e9408ce548
Cleiber Melo <66189758+cleibernerymelo@users.noreply.github.com>
2021-03-15 22:20:46 +0000

    arquivos principais jasmine node

---
+++ spec/exemplo/exemploSpec.js
@@ -0,0 +1,9 @@
+describe('Suite de tese de exemplo com NodeJs', () => {
+    it('deve demonstrar o uso do jasmine com o nodeJs', () => {
+        expect(true).toEqual(true)
+    });
+
+    it('deve demonstrar o uso do modo de estuca', () => {
+        expect(1).toEqual(1)
+    });
+});
